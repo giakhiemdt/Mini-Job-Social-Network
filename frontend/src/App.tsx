1a5720fcb852cca21d6c8ed59954c94edd23315d
PawPasta <giabaostrike2004@gmail.com>
2025-05-27 06:05:26 +0700

    import tailwind and daisy UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,19 @@
 
-
 function App() {
- 
+
 
   return (
     <>
-
-    
-
+      <div className="avatar">
+  <div className="w-24 rounded-xl">
+    <img src="https://img.daisyui.com/images/profile/demo/yellingwoman@192.webp" />
+  </div>
+</div>
+<div className="avatar">
+  <div className="w-24 rounded-full">
+    <img src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp" />
+  </div>
+</div>
     </>
   )
 }
